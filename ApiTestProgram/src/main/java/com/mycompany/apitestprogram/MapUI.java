ac3083bdb5e80fce454514a1ad241e6600779f3d
Jesus Alvarado <inkakola9@gmail.com>
2022-03-31 12:44:01 -0400

    Code for map UI

---
+++ ApiTestProgram/src/main/java/com/mycompany/apitestprogram/MapUI.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.apitestprogram;
+
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.geom.Point2D;
+import javax.swing.JFrame;
+import javax.swing.JToolTip;
+
+import org.jxmapviewer.JXMapKit;
+import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.OSMTileFactoryInfo;
+import org.jxmapviewer.viewer.DefaultTileFactory;
+import org.jxmapviewer.viewer.GeoPosition;
+import org.jxmapviewer.viewer.TileFactoryInfo;
+
+
+/**
+ *
+ * @author jesusalvarado
+ */
+public class MapUI {
+    
+
+    public static void main(String[] args)
+    {
+        //https://github.com/msteiger/jxmapviewer2
+        // my address 
+        //https://www.google.com/maps/place/141+Wantagh+Ave,+Levittown,+NY+11756/@40.7177811,-73.501066,17z/data=!3m1!4b1!4m5!3m4!1s0x89c27fda5db235d3:0xe8eafea191418970!8m2!3d40.7177811!4d-73.4988773
+       
+        //https://web.archive.org/web/20100309081606/http://today.java.net/pub/a/today/2007/10/30/building-maps-into-swing-app-with-jxmapviewer.html
+        //https://web.archive.org/web/20100311151851/http://www.openstreetmap.org/
+        
+        final JXMapKit jXMapKit = new JXMapKit();
+        TileFactoryInfo info = new OSMTileFactoryInfo();
+        DefaultTileFactory tileFactory = new DefaultTileFactory(info);
+        jXMapKit.setTileFactory(tileFactory);
+
+        //location of Java
+        //final GeoPosition gp = new GeoPosition(-7.502778, 111.263056);
+        //location of my home
+        
+        var p1 = 40.7128;
+        var p2 = -74.0060;
+        final GeoPosition gp = new GeoPosition(p1, p2);
+        
+
+        final JToolTip tooltip = new JToolTip();
+        //tooltip.add();
+        tooltip.setTipText("Pizz hut\nPoor Service\n2/5 stars :(");
+        tooltip.setComponent(jXMapKit.getMainMap());
+        jXMapKit.getMainMap().add(tooltip);
+
+        jXMapKit.setZoom(11);
+        jXMapKit.setAddressLocation(gp);
+
+        jXMapKit.getMainMap().addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) { 
+                // ignore
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e)
+            {
+                JXMapViewer map = jXMapKit.getMainMap();
+
+                // convert to world bitmap
+                Point2D worldPos = map.getTileFactory().geoToPixel(gp, map.getZoom());
+
+                // convert to screen
+                Rectangle rect = map.getViewportBounds();
+                int sx = (int) worldPos.getX() - rect.x;
+                int sy = (int) worldPos.getY() - rect.y;
+                Point screenPos = new Point(sx, sy);
+
+                // check if near the mouse
+                if (screenPos.distance(e.getPoint()) < 20)
+                {
+                    screenPos.x -= tooltip.getWidth() / 2;
+
+                    tooltip.setLocation(screenPos);
+                    tooltip.setVisible(true);
+                }
+                else
+                {
+                    tooltip.setVisible(false);
+                }
+            }
+        });
+
+        // Display the viewer in a JFrame
+        JFrame frame = new JFrame("JXMapviewer2 Example 6");
+        frame.getContentPane().add(jXMapKit);
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
