a3755b66764a17f819689d87e6b5d96f3ca9f1a6
Jon-CSC <98854693+Jon-CSC@users.noreply.github.com>
2022-04-05 14:47:49 -0400

    Reorganized code. Added method to check API by restaurant name. Added method to store full database as a JSON file.

--- ApiTestProgram/src/main/java/com/mycompany/apitestprogram/ApiTestProgram.java
+++ ApiTestProgram/src/main/java/com/mycompany/apitestprogram/ApiTestProgram.java
@@ -5,8 +5,11 @@ import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
 import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Testing API calls to the OpenData restaurant inspection database.
@@ -15,20 +18,14 @@ import java.util.Scanner;
  */
 public class ApiTestProgram {
 
-    public static void main(String[] args) {
+    /**
+     * Creates a local file containing all the data received from the full
+     * database. Slow, takes around 2 minutes to write all 360000 lines to file.
+     */
+    public static void updateLocalJSONData() {
         try {
-
-            //Test restuarant search by name
-            String searchName = "";
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Restaurant name:");
-            searchName = scan.next();
-
-            //Begin JSON
-            String jsonString = "";
-
-            // this is a test url, must use an API key in final code
-            URL url = new URL("https://data.cityofnewyork.us/resource/43nn-pn8j.json" + "?dba=" + searchName);
+            // Get ALL data in database
+            URL url = new URL("https://data.cityofnewyork.us/resource/43nn-pn8j.json?$limit=400000&$$app_token=Dxorb7ZOjkabbBiII4JMJhkQu");// + "?dba=" + searchName);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             // open connection
@@ -36,13 +33,53 @@ public class ApiTestProgram {
             // read response
             BufferedReader input = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String inputLine;
+            System.out.println("Response received.");
+            PrintWriter writer = new PrintWriter("localData.json", "UTF-8");
+
+            while ((inputLine = input.readLine()) != null) {
+                writer.println(inputLine);
+                System.out.println("...");
+            }
+            writer.close();
+        } catch (MalformedURLException ex ) {
+            Logger.getLogger(ApiTestProgram.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ApiTestProgram.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * Queries the database API using a restaurant name, getting the first 1000 
+     * results and prints them out to System.out.
+     * @param restaurantName The restaurant name.
+     */
+    public static void getDataFromName(String restaurantName) {
+        try {
+            //Begin JSON
+            String jsonString = "";
+
+            // App token is Dxorb7ZOjkabbBiII4JMJhkQu
+            URL url = new URL("https://data.cityofnewyork.us/resource/43nn-pn8j.json" 
+                    + "?dba=" + restaurantName 
+                    + "&$limit=1000"
+                            + "&$$app_token=Dxorb7ZOjkabbBiII4JMJhkQu");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            // open connection
+            conn.connect();
+            // read response
+            BufferedReader input = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            System.out.println("Response received.");
+
             while ((inputLine = input.readLine()) != null) {
                 jsonString += inputLine;
             }
+
             // put response into json array
             JsonArray response = (JsonArray) new JsonParser().parse(jsonString);
             // iterate over the array to process each line
-            System.out.println("Latitude and Longitudes of " + searchName);
+            System.out.println("Latitude and Longitudes of " + restaurantName);
             for (int i = 0; i < response.size(); i++) {
 
                 //some entries seem to not have lat long, printing those here
@@ -61,9 +98,21 @@ public class ApiTestProgram {
             // close connection
             conn.disconnect();
 
-        } catch (JsonSyntaxException | IOException ex) {
+        } catch (JsonSyntaxException ex) {
             System.err.println(ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ApiTestProgram.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-}
 
+    public static void main(String[] args) {
+            //Test restaurant search by name
+            System.out.println("Restaurant name:");
+            Scanner scan = new Scanner(System.in);
+            String searchName = scan.nextLine();
+            getDataFromName(searchName);
+            
+            //Store full database locally in JSON text file
+            //updateLocalJSONData();
+    }
+}
