2b8c653e98e3376033ebebf0b5072dbb585547e1
ndars <89598232+ndars@users.noreply.github.com>
2022-04-12 11:45:38 -0400

    Removed unused imports

--- RestaurantMap/src/main/java/com/groupfour/restaurantmap/APIDataFetcher.java
+++ RestaurantMap/src/main/java/com/groupfour/restaurantmap/APIDataFetcher.java
@@ -6,8 +6,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.logging.Level;
