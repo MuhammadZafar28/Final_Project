3f2f8f3f31122a8781ef23137c5778be7e31b7e0
Omar Muy <muyof@farmingdale.edu>
2022-04-12 18:05:49 -0400

    Made some test cases and practicing getting use to SoQL format

--- ApiTestProgram/src/main/java/com/mycompany/apitestprogram/ApiTestProgram.java
+++ ApiTestProgram/src/main/java/com/mycompany/apitestprogram/ApiTestProgram.java
@@ -41,16 +41,17 @@ public class ApiTestProgram {
                 System.out.println("...");
             }
             writer.close();
-        } catch (MalformedURLException ex ) {
+        } catch (MalformedURLException ex) {
             Logger.getLogger(ApiTestProgram.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(ApiTestProgram.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     /**
-     * Queries the database API using a restaurant name, getting the first 1000 
+     * Queries the database API using a restaurant name, getting the first 1000
      * results and prints them out to System.out.
+     *
      * @param restaurantName The restaurant name.
      */
     public static void getDataFromName(String restaurantName) {
@@ -59,10 +60,10 @@ public class ApiTestProgram {
             String jsonString = "";
 
             // App token is Dxorb7ZOjkabbBiII4JMJhkQu
-            URL url = new URL("https://data.cityofnewyork.us/resource/43nn-pn8j.json" 
-                    + "?dba=" + restaurantName 
+            URL url = new URL("https://data.cityofnewyork.us/resource/43nn-pn8j.json"
+                    + "?dba=" + restaurantName
                     + "&$limit=1000"
-                            + "&$$app_token=Dxorb7ZOjkabbBiII4JMJhkQu");
+                    + "&$$app_token=Dxorb7ZOjkabbBiII4JMJhkQu");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             // open connection
@@ -105,14 +106,82 @@ public class ApiTestProgram {
         }
     }
 
-    public static void main(String[] args) {
-            //Test restaurant search by name
-            System.out.println("Restaurant name:");
-            Scanner scan = new Scanner(System.in);
-            String searchName = scan.nextLine();
-            getDataFromName(searchName);
-            
-            //Store full database locally in JSON text file
-            //updateLocalJSONData();
+    /*  
+    @author Omar 
+    Testing out filtering in SoQL 
+     */
+    public static void runTestFilter() {
+        System.out.println("Choose a # based on which restaurants you'd want to see based on borough ");
+        System.out.println("1 - Manhattan, 2 - Brooklyn, 3 - Bronx, 4 - Queens, 5 - Staten Island ");
+        Scanner s = new Scanner(System.in);
+        int userInput = s.nextInt();
+        getSpecificFilter(userInput);
+
     }
+
+    /*
+    @author Omar
+    Specfic Filter for BORO
+     */
+    public static void getSpecificFilter(int userInput) {
+        switch (userInput) {
+            case 1: {
+                try {
+                    String jsonString = "";
+
+                    URL url = new URL("https://data.cityofnewyork.us/resource/43nn-pn8j.json?$select=dba&$where=dba=Manhattan&$limit=1000&$$app_token=Dxorb7ZOjkabbBiII4JMJhkQu");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    // open connection
+                    conn.connect();
+                    // read response
+                    BufferedReader input = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    String inputLine;
+                    System.out.println("Done");
+
+                    while ((inputLine = input.readLine()) != null) {
+                        jsonString += inputLine;
+                    }
+                    // put response into json array
+                    JsonArray response = (JsonArray) new JsonParser().parse(jsonString);
+                    
+                    
+                    // close connection
+                    conn.disconnect();
+                } catch (MalformedURLException ex) {
+                    Logger.getLogger(ApiTestProgram.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(ApiTestProgram.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            System.out.println("Here are the restaurants in Manhattan");
+
+            case 2:
+                System.out.println("Here are the restaurants in Brooklyn");
+            case 3:
+                System.out.println("Here are the restaurants in Bronx");
+            case 4:
+                System.out.println("Here are the restaurants in Queens");
+            case 5:
+                System.out.println("Here are the restaurants in Staten Island");
+            default:
+        }
+        System.out.println("Terminate");
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        // Testing Filtering
+        //runTestFilter();
+        //Test restaurant search by name
+        System.out.println("Restaurant name:");            //Test restaurant search by name
+
+        Scanner scan = new Scanner(System.in);
+        String searchName = scan.nextLine();
+        getDataFromName(searchName);
+
+        //Store full database locally in JSON text file
+        //updateLocalJSONData();
+    }
+
 }
