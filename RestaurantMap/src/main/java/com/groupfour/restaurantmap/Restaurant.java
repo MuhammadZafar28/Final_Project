683a30e630cef2f5ad0d61ca5db574907b332d1a
ndars <89598232+ndars@users.noreply.github.com>
2022-04-23 15:07:35 -0400

    Added to Restaurant implementation

--- RestaurantMap/src/main/java/com/groupfour/restaurantmap/Restaurant.java
+++ RestaurantMap/src/main/java/com/groupfour/restaurantmap/Restaurant.java
@@ -1,10 +1,13 @@
 package com.groupfour.restaurantmap;
 
+import java.util.ArrayList;
+
 /**
  * A class which stores DOHMH restaurant and inspection data.
  * @author ndars
  */
 public class Restaurant {
+
     private int CAMIS;
     private String DBA;
     private Borough borough;
@@ -13,7 +16,28 @@ public class Restaurant {
     private String zipCode;
     private String phone;
     private String foodType;
-    //private SomeKindOfCollection<Inspection> inspections
+    private double latitude;
+    private double longitude;
+    private ArrayList<Inspection> inspections;
+    
+    // true if this is a shallow object; i.e. no inspection data is stored, just biographical
+    private boolean isShallow;
+    
+    public Restaurant(int CAMIS, String DBA, Borough borough, String building, String street, 
+                    String zipCode, String phone, String foodType, 
+                    double latitude, double longitude, boolean isShallow) {
+        this.CAMIS = CAMIS;
+        this.DBA = DBA;
+        this.borough = borough;
+        this.building = building;
+        this.street = street;
+        this.zipCode = zipCode;
+        this.phone = phone;
+        this.foodType = foodType;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.isShallow = isShallow;
+    }
     
     @Override
     public int hashCode() {
@@ -51,4 +75,34 @@ public class Restaurant {
     public String getFoodType() {
         return foodType;
     }
+    
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+    
+    public ArrayList<Inspection> getInspections() throws ShallowObjectException {
+        if(isShallow) {
+            throw new ShallowObjectException();
+        } else {
+            return inspections;
+        }
+    }
+    
+    public void setInspections(ArrayList<Inspection> inspections) {
+        if(inspections == null) {
+            return;
+        }
+        isShallow = false;
+        this.inspections = inspections;
+    }
+    
+    public boolean isShallow() {
+        return isShallow;
+    }
 }
+
+class ShallowObjectException extends Exception {}
