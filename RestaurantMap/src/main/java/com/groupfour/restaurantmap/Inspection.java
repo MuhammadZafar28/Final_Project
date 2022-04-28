da588d16ee4fb4b5a31981fbc6eb7128411339b9
ndars <89598232+ndars@users.noreply.github.com>
2022-04-28 12:00:14 -0400

    Added documentation and functionality for violation codes

--- RestaurantMap/src/main/java/com/groupfour/restaurantmap/Inspection.java
+++ RestaurantMap/src/main/java/com/groupfour/restaurantmap/Inspection.java
@@ -1,6 +1,8 @@
 package com.groupfour.restaurantmap;
 
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.Set;
 
 /**
  * A class which stores data for a particular DOHMH restaurant inspection.
@@ -12,7 +14,7 @@ public class Inspection implements Comparable<Inspection> {
     private final int CAMIS; // the restaurant whose inspection this is
     private final String date; // the date of the inspection
     private final String inspectionType; // the inspection type; e.g. cycle, administrative, etc.
-    
+
     private final String action; // the action taken as a result of the inspection
     private final Grade grade;
     private final int score;
@@ -43,17 +45,51 @@ public class Inspection implements Comparable<Inspection> {
         return inspectionType;
     }
     
-    public void addViolationCode(String code) throws InvalidViolationException {
-        // if it's canonical add it
-        // if it's not throw the exception
+    public String getAction() {
+        return action;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * Returns an unmodifiable set of this inspection's violation codes.
+     * If you want to add to the set, use {@link #addViolation(java.lang.String) addViolation} instead.
+     * @return 
+     */
+    public Set<String> lookAtViolationCodes() {
+        return Collections.unmodifiableSet(violationCodes);
     }
     
+    /**
+     * Adds the given violation code to this inspection's violations.
+     * @param code
+     * @throws InvalidViolationException if the code does not map to an existing violation in {@link com.groupfour.restaurantmap.ViolationTable ViolationTable}.
+     */
+    public void addViolation(String code) throws InvalidViolationException {
+       if(ViolationTable.isValidCode(code)) {
+           violationCodes.add(code);
+       } else {
+           throw new InvalidViolationException();
+       }
+    }
+        
     @Override
     public int hashCode() {
         long rawHash = CAMIS + date.hashCode() + (inspectionType.hashCode() - date.hashCode())/2;
         return (int)(rawHash % Integer.MAX_VALUE);
     }
     
+    /**
+     * Returns true iff the restaurant, the date, and the inspection type are all the same.
+     * @param obj
+     * @return 
+     */
     @Override
     public boolean equals(Object obj) {
         if(obj == this) {
@@ -68,6 +104,11 @@ public class Inspection implements Comparable<Inspection> {
                 (insp.getInspectionType().equals(this.inspectionType));
     }
 
+    /**
+     * Orders inspections by date.
+     * @param insp
+     * @return -1 if this is more recent, 1 if insp is more recent, 0 if same date.
+     */
     @Override
     public int compareTo(Inspection insp) {
         if(insp == this) {
@@ -76,5 +117,3 @@ public class Inspection implements Comparable<Inspection> {
         return -1*(insp.getDate().compareTo(this.date));
     }
 }
-
-class InvalidViolationException extends Exception {}
