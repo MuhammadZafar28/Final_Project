f4250a4b26504f91f8419749fc3f1282a18d1b4a
Jon-CSC <98854693+Jon-CSC@users.noreply.github.com>
2022-05-05 17:17:44 -0400

    Wrote statements and comments for possible query request format based on specificity. The idea is to check how specific the incoming info is and build a query from the more specific combination to the to the least specific combination.

--- RestaurantMap/src/main/java/com/groupfour/restaurantmap/APIQuery.java
+++ RestaurantMap/src/main/java/com/groupfour/restaurantmap/APIQuery.java
@@ -1,17 +1,71 @@
 package com.groupfour.restaurantmap;
 
 /**
- * An class representing API queries to the DOHMH restaurant inspection database.
+ * An class representing API queries to the DOHMH restaurant inspection
+ * database.
+ *
  * @author ndars
  */
 public class APIQuery extends Query {
-    // App token is Dxorb7ZOjkabbBiII4JMJhkQu
+
     private final String endpoint = "https://data.cityofnewyork.us/resource/43nn-pn8j.json";
-    
+    /* App token for our program isis Dxorb7ZOjkabbBiII4JMJhkQu, must be 
+    included at the end of the final URL.*/
+    private final String apiKey = "Dxorb7ZOjkabbBiII4JMJhkQu";
+
     public APIQuery(String dba, Grade grade, String foodType,
             String building, String street, Borough borough, String zipCode) {
+
+        // Choose query based on info provided, most specific to least
         
-//        
+        // A specific restaurant at an exact address and name
+        if (building != null && street != null && zipCode != null
+                && dba != null) {}
+        /* Any restaurant at an exact address and grade, but no specific 
+        name (ie, the food court in a mall)*/ 
+        else if (building != null && street != null && zipCode != null
+                && grade != null) {} 
+        /* Any restaurant at an exact address and food type, but no specific 
+        name (ie, the food court in a mall*/  
+        else if (building != null && street != null && zipCode != null
+                && foodType != null) {}
+        // Any restaurant at an exact address, regardless of grade or food type 
+        else if (building != null && street != null && zipCode != null) {} 
+        // A specific restaurant on a specific street & zip
+        else if (street != null && zipCode != null && dba != null) {}  
+        // Any restaurant on a specific street with a specific grade
+        else if (street != null && zipCode != null && grade != null) {} 
+        // Any restaurant on a specific street with a specific cuisine type
+        else if (street != null && zipCode != null && foodType != null) {} 
+        // All restaurants on a specific street
+        else if (street != null && zipCode != null) {} 
+        // A specific restaurant name within a specific zip
+        else if (zipCode != null && dba != null) {}
+        // Any restaurant in a specific zipcode with a specific grade 
+        else if (zipCode != null && grade != null) {}
+        // Any restaurant in a specific zipcode with a specific cuisine type 
+        else if (zipCode != null && foodType != null) {} 
+        // All restaurants within a specific zip code
+        else if (zipCode != null) {} 
+        // A specific restaurant name within a specific borough
+        else if (borough != null && dba != null) {} 
+        // Any restaurant in a specific borough with a specific grade
+        else if (borough != null && grade != null) {} 
+        // Any restaurant in a specific borough with a specific cuisine type
+        else if (borough != null && foodType != null) {} 
+        // All restaurants within a specific borough
+        else if (borough != null) {}
+        // All restaurants of a specific name and grade within the database
+        else if(dba != null && grade != null){}
+        // All restaurants with a specific cuisine and grade within the database
+        else if(foodType != null && grade != null){}
+        // All restaurants with a specific name within the database
+        else if(dba != null){}
+        // All restaurants with a specific cuisine type within the database
+        else if(foodType != null){}
+        // All restaurants with a specific grade within the database
+        else if(grade != null){}
+//      
 //        StringBuilder sb = new StringBuilder();
 //        sb.append(endpoint).append("?");
 //        if(null != dba) {
