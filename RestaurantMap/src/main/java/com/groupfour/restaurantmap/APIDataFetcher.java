fc5e1c63f1bface271066599c3ddc0d3124bbf82
ndars <89598232+ndars@users.noreply.github.com>
2022-05-12 13:10:30 -0400

    Adjusted to fit DataFetcher definition & added parse code

--- RestaurantMap/src/main/java/com/groupfour/restaurantmap/APIDataFetcher.java
+++ RestaurantMap/src/main/java/com/groupfour/restaurantmap/APIDataFetcher.java
@@ -1,6 +1,8 @@
 package com.groupfour.restaurantmap;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,8 +19,15 @@ import java.util.logging.Logger;
  */
 public class APIDataFetcher implements DataFetcher {
 
+    /**
+     * Fetches shallow restaurant data for a query targeting zero to many restaurants.
+     * Query must be formatted to grab biographical data matching the fields of Restaurant.
+     * @param query 
+     * @return a HashMap of Restaurants keyed by each Restaurant's CAMIS
+     * @throws IllegalArgumentException if Query is not an APIQuery
+     */
     @Override
-    public HashMap<Integer, Restaurant> fetchData(Query query) throws IllegalArgumentException {
+    public HashMap<Integer, Restaurant> fetchRestaurants(Query query) throws IllegalArgumentException {
         if(null == query) {
             throw new IllegalArgumentException("Fetcher cannot use null queries.");
         }
@@ -27,11 +36,11 @@ public class APIDataFetcher implements DataFetcher {
                     + "Required: APIQuery | Found: " + query.getClass());
         }
         
+        HashMap<Integer, Restaurant> restaurants = new HashMap<>();
         try {
             URL queryURL = new URL(query.toString());
             HttpURLConnection conn = (HttpURLConnection) queryURL.openConnection();
             conn.setRequestMethod("GET");
-            // open connection
             conn.connect();
             // read response
             BufferedReader input = new BufferedReader(new InputStreamReader(conn.getInputStream()));
@@ -45,13 +54,44 @@ public class APIDataFetcher implements DataFetcher {
 
             // put response into json array
             JsonArray response = (JsonArray) new JsonParser().parse(jsonString);
-            // iterate over the array to process each line
             for (int i = 0; i < response.size(); i++) {
-                System.out.println(response.get(i));
+                JsonObject line = response.get(i).getAsJsonObject();
+                System.out.println(line);
+                JsonElement camis = line.get("camis");
+                JsonElement dba = line.get("dba");
+                JsonElement borough = line.get("borough");
+                JsonElement building = line.get("building");
+                JsonElement street = line.get("street");
+                JsonElement zipCode = line.get("zipcode");
+                JsonElement phone = line.get("phone");
+                JsonElement latitude = line.get("latitude");
+                JsonElement longitude = line.get("longitude");
+                JsonElement foodType = line.get("cuisine_description");
+                JsonElement grade = line.get("grade");
             }
         } catch (IOException ex) {
             Logger.getLogger(APIDataFetcher.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return null;
+        return restaurants;
     }   
+    
+    /**
+     * Fetches deep inspeciton data for a query targeting a single restaurant.
+     * Query must be formatted to grab inspection data matching the fields of Inspection.
+     * @param query
+     * @return a single, deep Restaurant object containing all of the Restaurant's inspection data
+     * @throws IllegalArgumentException if Query is not an APIQuery
+     */
+    @Override
+    public Restaurant fetchRestaurant(Query query) throws IllegalArgumentException {
+        if(null == query) {
+            throw new IllegalArgumentException("Fetcher cannot use null queries.");
+        }
+        if(!(query instanceof APIQuery)) {
+            throw new IllegalArgumentException("Wrong query format for this fetcher! "
+                    + "Required: APIQuery | Found: " + query.getClass());
+        }
+        
+        return null;
+    }
 }
