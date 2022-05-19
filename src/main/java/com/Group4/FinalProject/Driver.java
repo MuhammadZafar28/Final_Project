69cc007881b5656c6ef171161a0525113413e3cc
MuyOmar <muyof@farmingdale.edu>
2022-05-19 01:42:27 -0400

    Creating a Java Spring Boot application to be used alongside Angular. Correctly working on the service aspect of the RestTemplate.

---
+++ src/main/java/com/Group4/FinalProject/Driver.java
@@ -0,0 +1,13 @@
+package com.Group4.FinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Driver {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Driver.class, args);
+	}
+
+}
